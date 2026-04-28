1bbbf2f929fa9c94a955628dfb2d29bbbeec23c6
brunoferreira126 <ferreira_brunomendes@hotmail.com>
2026-04-28 15:55:07 -0300

    Landing Bussola

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -51,7 +51,7 @@ export default function Hero() {
             */}
 
             <a
-              href="https://wa.me/558589411912"
+              href="https://wa.me/558584241536"
               target="_blank"
               rel="noreferrer"
               className="btn-whatsapp"
